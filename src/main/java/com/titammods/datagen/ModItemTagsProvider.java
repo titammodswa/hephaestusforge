6551c5321c1f0850ecd760fe49dd2c5a7ec86d14
titammods <xtutoriais014@gmail.com>
2026-07-01 20:57:20 -0300

    fixed tags issure

--- src/main/java/com/titammods/datagen/ModItemTagsProvider.java
+++ src/main/java/com/titammods/datagen/ModItemTagsProvider.java
@@ -3,7 +3,6 @@ package com.titammods.datagen;
 import com.titammods.TitamMods;
 import com.titammods.setup.ModItems;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.resources.ResourceLocation;
@@ -59,65 +58,38 @@ public class ModItemTagsProvider extends ItemTagsProvider {
         };
         String[] atoVanillaCompat = {"iron", "gold", "copper", "diamond"};
 
-        var clumpsTag = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "clumps"));
-        var crystalsTag = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "crystals"));
+        var clumpsTag    = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "clumps"));
+        var crystalsTag  = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "crystals"));
         var dirtyDustsTag = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "dirty_dusts"));
-        var shardsTag = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "shards"));
+        var shardsTag    = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "shards"));
         var oreHammersTag = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ore_hammers"));
 
-        for (String name : atoMetals) {
-            var hammer = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("alltheores", name + "_ore_hammer"));
-            if (hammer != Items.AIR) tag(oreHammersTag).add(hammer);
-        }
-        for (String name : atoVanillaCompat) {
-            var hammer = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("alltheores", name + "_ore_hammer"));
-            if (hammer != Items.AIR) tag(oreHammersTag).add(hammer);
+        String[] allAto = {"aluminum", "bronze", "constantan", "electrum", "enderium", "invar", "iridium",
+                "lead", "lumium", "nickel", "osmium", "platinum", "signalum", "silver", "tin", "uranium",
+                "iron", "gold", "copper", "diamond"};
+
+        String[] hammerPrefixes = {"bronze", "invar", "platinum", "iron", "copper"};
+        for (String name : hammerPrefixes) {
+            tag(oreHammersTag).addOptional(ResourceLocation.fromNamespaceAndPath("alltheores", name + "_ore_hammer"));
         }
 
-        for (String name : atoMetals) {
-            var clump = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("alltheores", name + "_clump"));
-            if (clump != Items.AIR) {
-                tag(clumpsTag).add(clump);
-                tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "clumps/" + name))).add(clump);
-            }
-            var crystal = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("alltheores", name + "_crystal"));
-            if (crystal != Items.AIR) {
-                tag(crystalsTag).add(crystal);
-                tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "crystals/" + name))).add(crystal);
-            }
-            var dirtyDust = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("alltheores", "dirty_" + name + "_dust"));
-            if (dirtyDust != Items.AIR) {
-                tag(dirtyDustsTag).add(dirtyDust);
-                tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "dirty_dusts/" + name))).add(dirtyDust);
-            }
-            var shard = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("alltheores", name + "_shard"));
-            if (shard != Items.AIR) {
-                tag(shardsTag).add(shard);
-                tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "shards/" + name))).add(shard);
-            }
-        }
+        for (String name : allAto) {
+            var clumpId     = ResourceLocation.fromNamespaceAndPath("alltheores", name + "_clump");
+            var crystalId   = ResourceLocation.fromNamespaceAndPath("alltheores", name + "_crystal");
+            var dirtyDustId = ResourceLocation.fromNamespaceAndPath("alltheores", "dirty_" + name + "_dust");
+            var shardId     = ResourceLocation.fromNamespaceAndPath("alltheores", name + "_shard");
 
-        for (String name : atoVanillaCompat) {
-            var clump = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("alltheores", name + "_clump"));
-            if (clump != Items.AIR) {
-                tag(clumpsTag).add(clump);
-                tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "clumps/" + name))).add(clump);
-            }
-            var crystal = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("alltheores", name + "_crystal"));
-            if (crystal != Items.AIR) {
-                tag(crystalsTag).add(crystal);
-                tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "crystals/" + name))).add(crystal);
-            }
-            var dirtyDust = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("alltheores", "dirty_" + name + "_dust"));
-            if (dirtyDust != Items.AIR) {
-                tag(dirtyDustsTag).add(dirtyDust);
-                tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "dirty_dusts/" + name))).add(dirtyDust);
-            }
-            var shard = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("alltheores", name + "_shard"));
-            if (shard != Items.AIR) {
-                tag(shardsTag).add(shard);
-                tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "shards/" + name))).add(shard);
-            }
+            tag(clumpsTag).addOptional(clumpId);
+            tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "clumps/" + name))).addOptional(clumpId);
+
+            tag(crystalsTag).addOptional(crystalId);
+            tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "crystals/" + name))).addOptional(crystalId);
+
+            tag(dirtyDustsTag).addOptional(dirtyDustId);
+            tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "dirty_dusts/" + name))).addOptional(dirtyDustId);
+
+            tag(shardsTag).addOptional(shardId);
+            tag(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "shards/" + name))).addOptional(shardId);
         }
     }
 }
